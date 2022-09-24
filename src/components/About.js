caeda5693906a008906abe61b79497d52687dd07
CeciliaBdev <cecilia.bernard@live.fr>
2022-09-24 20:58:55 +0200

    ajout composant drop sur la page About

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,43 @@
 import NavBar from './Navbar'
+import Dropdown from './Dropdown'
+
 import '../styles/About.css'
 
 function About() {
+  const values = [
+    {
+      id: 1,
+      title: 'Fiabilité',
+      text: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes',
+    },
+    {
+      id: 2,
+      title: 'Respect',
+      text: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exlusion de notre plateforme.',
+    },
+    {
+      id: 3,
+      title: 'Service',
+      text: 'Nos équipes se tiennet à votre disposition pour vous fournir une expérience parfaite. N hésitez pas à nous contacter si vous avez la moindre question.',
+    },
+    {
+      id: 4,
+      title: 'Sécurité',
+      text: 'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l hôte qu au locataire, cela permet à nos équipes de vérifier que les stadards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes. ',
+    },
+  ]
+  console.log(values)
+
   return (
-    <div>
+    <div className="container-about">
       <NavBar />
       <div className="about-bg"></div>
+      <div>
+        {/* <Dropdown /> */}
+        {values.map((dropdown) => {
+          return <Dropdown key={dropdown.id} {...dropdown} />
+        })}
+      </div>
     </div>
   )
 }
---
+++ src/components/Dropdown.js
@@ -0,0 +1,26 @@
+import '../styles/Dropdown.css'
+import { useState } from 'react'
+
+function Dropdown(drop) {
+  const [display, setDisplay] = useState('none')
+  function handleClick() {
+    if (display === 'none') {
+      setDisplay('block')
+    } else {
+      setDisplay('none')
+    }
+  }
+  return (
+    <div className="container-drop">
+      <div onClick={() => handleClick()} className="dropdown-menu">
+        <p>{drop.title}</p>
+      </div>
+      <div style={{ display: display }}>
+        <div className="text-dropdown">
+          <p>{drop.text}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Dropdown
