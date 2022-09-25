85e8d5b248980334e83cba842485d16d7efc4547
CeciliaBdev <cecilia.bernard@live.fr>
2022-09-25 21:12:26 +0200

    rotation chevron dropdown ok

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -10,10 +10,30 @@ function Dropdown(drop) {
       setDisplay('none')
     }
   }
+  const [rotateChevron, setRotateChevron] = useState('rotate(0)')
+  function Rotate() {
+    if (rotateChevron === 'rotate(0)') {
+      setRotateChevron('rotate(180deg)')
+    } else {
+      setRotateChevron('rotate(0)')
+    }
+  }
+
   return (
     <div className="container-drop">
-      <div onClick={() => handleClick()} className="dropdown-menu">
-        <p>{drop.title}</p> <i className="fas fa-chevron-down"></i>
+      <div
+        onClick={() => {
+          handleClick()
+          Rotate()
+        }}
+        className="dropdown-menu"
+      >
+        <p>{drop.title}</p>{' '}
+        <i
+          style={{ transform: rotateChevron }}
+          //   onClick={() => Rotate()}
+          className="fas fa-chevron-down"
+        ></i>
       </div>
       <div style={{ display: display }}>
         <div className="text-dropdown">
