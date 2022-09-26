f9ca53eb51bb54a2c87c6b69d0d4e138da31cb66
CeciliaBdev <cecilia.bernard@live.fr>
2022-09-26 12:23:15 +0200

    update tags ok

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,5 +1,10 @@
-function Tag() {
-  return <div></div>
+import '../styles/Tags.css'
+function Tag(props) {
+  return (
+    <div className="container-tags">
+      <p className="tag">{props.tagsNom}</p>
+    </div>
+  )
 }
 
 export default Tag
