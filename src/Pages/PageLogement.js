02aa05e15e92fc8385367eb3bd7486527db94268
CeciliaBdev <cecilia.bernard@live.fr>
2022-09-26 21:11:51 +0200

    update css fiche logement

--- src/Pages/PageLogement.js
+++ src/Pages/PageLogement.js
@@ -3,8 +3,9 @@ import Locations from '../datas/locations.json'
 import Tag from '../components/Tags'
 import DropdownDescription from '../components/DropdownDescription'
 import DropdownEquipement from '../components/DropdownEquipement'
-import Error from '../Pages/Error'
-import Lightbox from '../components/Lightbox'
+// import Stars from '../components/Stars'
+// import Error from '../Pages/Error'
+// import Lightbox from '../components/Lightbox'
 import '../styles/PageLogement.css'
 
 function PageLogement() {
@@ -19,11 +20,32 @@ function PageLogement() {
         <i className="fas fa-chevron-right"></i>
       </div>
 
-      <h2>{logement.title}</h2>
-      <h3>{logement.location}</h3>
-      {logement.tags.map((tag) => (
-        <Tag tagsNom={tag} key={tag} />
-      ))}
+      <div className="container-infos">
+        {/* infos */}
+        <div className="Infos">
+          <h2>{logement.title}</h2>
+          <h3>{logement.location}</h3>
+          {logement.tags.map((tag) => (
+            <Tag tagsNom={tag} key={tag} />
+          ))}
+        </div>
+
+        <div className="container-host">
+          {/* host */}
+          <div className="name">
+            <div>
+              <p>{logement.host.name.split(' ')[0]}</p>
+
+              <p>{logement.host.name.split(' ')[1]}</p>
+            </div>
+          </div>
+          <div className="profil">
+            <img src={logement.host.picture} alt="" />
+          </div>
+        </div>
+        {/* {logement.rating} */}
+      </div>
+      {/* drops */}
       <div className="dropdown">
         <DropdownDescription key={logement.id} {...logement} />
         <DropdownEquipement key={logement.id} {...logement} />
