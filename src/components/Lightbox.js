6c807ce8b5a2f7f55c8fab8718bfd113694ea1c3
CeciliaBdev <cecilia.bernard@live.fr>
2022-09-27 15:49:00 +0200

    fonction lightbox ok

--- src/components/Lightbox.js
+++ src/components/Lightbox.js
@@ -1,9 +1,22 @@
+import '../styles/Lightbox.css'
+import { useState } from 'react'
+
 function Lightbox({ pictures }) {
+  const [index, updateIndex] = useState(0)
+  console.log(pictures)
   const Prev = () => {
     console.log('click precedent')
+    // si index => 1er element
+    // revient à la derniere photo
+    // sinon photo precedente
+    index === 0 ? updateIndex(pictures.length - 1) : updateIndex(index - 1)
   }
   const Suiv = () => {
-    console.log('click suivant')
+    // console.log('click suivant')
+    // si index => dernier element
+    // reviennt à la premier photo
+    // sinon photo suivante
+    index === pictures.length - 1 ? updateIndex(0) : updateIndex(index + 1)
   }
   return (
     <div>
@@ -14,7 +27,7 @@ function Lightbox({ pictures }) {
             Prev()
           }}
         ></i>
-        <img src={pictures[0]} alt="" />
+        <img src={pictures[index]} alt="" />
         <i
           className="fas fa-chevron-right boutonSuiv"
           onClick={() => {
