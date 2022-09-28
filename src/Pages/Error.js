ce5f1021be508920f365bee537aec0202eaf1855
CeciliaBdev <cecilia.bernard@live.fr>
2022-09-28 15:57:49 +0200

    update css mobile page erreur

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,11 +1,9 @@
 import '../styles/Error.css'
 import { Link } from 'react-router-dom'
-import NavBar from '../components/Navbar'
 
 function Error() {
   return (
     <div className="error">
-      <NavBar />
       <p className="number">404</p>
       <p className="texte"> Oups ! La page que vous demandez n'existe pas</p>
       <nav className="navError">
