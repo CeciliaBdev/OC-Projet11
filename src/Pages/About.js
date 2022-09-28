f7020a461b6e01dc044885a31a48b4b6ab963366
CeciliaBdev <cecilia.bernard@live.fr>
2022-09-28 11:19:25 +0200

    page a propos css ok

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -18,12 +18,12 @@ function About() {
     {
       id: 3,
       title: 'Service',
-      text: 'Nos équipes se tiennet à votre disposition pour vous fournir une expérience parfaite. N hésitez pas à nous contacter si vous avez la moindre question.',
+      text: 'Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N hésitez pas à nous contacter si vous avez la moindre question.',
     },
     {
       id: 4,
       title: 'Sécurité',
-      text: 'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l hôte qu au locataire, cela permet à nos équipes de vérifier que les stadards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes. ',
+      text: 'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l hôte qu au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes. ',
     },
   ]
   // console.log(values)
